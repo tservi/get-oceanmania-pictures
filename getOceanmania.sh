70b936c0b257ce53adc819c02e5fdc4453db5993
tservi <jeantinguelyawais@gmail.com>
2010-11-09 23:23:59 +0000

    the source code...

--- getOceanmania.sh
+++ getOceanmania.sh
@@ -18,3 +18,11 @@ do
  fi
 
 done
+wget "http://www.oceanmania.ch/hub/files/quiz_041-046.jpg"
+wget "http://www.oceanmania.ch/hub/files/quiz_108-113.jpg"
+wget "http://www.oceanmania.ch/hub/files/quiz_132-137.jpg"
+wget "http://www.oceanmania.ch/hub/files/quiz_119-1.jpg"
+wget "http://www.oceanmania.ch/hub/files/quiz_092-1.jpg"
+wget "http://www.oceanmania.ch/hub/files/quiz_087-1.jpg"
+
+
