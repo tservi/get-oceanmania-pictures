455722f6975fda34c31ea07451103c29f1ab2d51
tservi <jeantinguelyawais@gmail.com>
2010-11-11 17:28:01 +0000

    new source code shorter with seq - thx BOFH

--- getOceanmania.sh
+++ getOceanmania.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
 
-i=0
-while [ $i -lt 180 ]
-do
- i=`expr $i + 1`
-
- if [ $i -lt 10 ]
-	then
-		wget "http://www.oceanmania.ch/hub/files/quiz_00$i.jpg"
-	else
-		if [ $i -lt 100 ]
-			then
-				wget "http://www.oceanmania.ch/hub/files/quiz_0$i.jpg"
-			else
-				wget "http://www.oceanmania.ch/hub/files/quiz_$i.jpg"
-		fi
- fi
-
+# modified by BOFH
+for i in $(seq -w 1 180)
+do 
+  wget "http://www.oceanmania.ch/hub/files/quiz_$i.jpg"
 done
 wget "http://www.oceanmania.ch/hub/files/quiz_041-046.jpg"
 wget "http://www.oceanmania.ch/hub/files/quiz_108-113.jpg"
